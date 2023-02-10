c42e6a3aba2c2f8e8203800261869056911f1dfe
fransua <serra.francois@gmail.com>
2023-02-10 13:15:03 +0100

    counter per droplet updated to count empty droplets too

--- scripts/count_mapped_per_cell.sh
+++ scripts/count_mapped_per_cell.sh
@@ -11,10 +11,6 @@ cpu=8
 Help()
 {
    # Display Help
-   echo "Add description of the script functions here."
-   echo 
-   echo "WARNING: before anything clean BAM should be generated with:"
-   echo "  $ samtools view -@ 20 -h -b -f 2 -F 1024 possorted_bam.bam > possorted_uniq_mapped_proper.bam"
    echo
    echo "Syntax: ./count_mapped_per_cell.sh [-i|c|h]"
    echo "options:"
@@ -22,25 +18,49 @@ Help()
    echo "c     number CPUs to use (default $cpu)"
    echo "h     Print this Help."
    echo
+   exit 1;
 }
 
 
-while getopts ":hi:" option; do
-    case $option in 
+while getopts ":h:i:c:" option; do
+    case ${option} in 
     h)
         Help;;
     i)
         rep=$OPTARG;;
     c)
         cpu=$OPTARG;;
+    *)
+        Help;;
     esac
 done
 
-# clean
+if [ -z "$rep" ]; then 
+    echo "ERROR: missing input directory..."
+    Help
+fi
+
+# get uniquely mapped reads:
+echo "Getting uniquely mapped reads from cellranger output"
+samtools view -@ $cpu -h -b -f 2 -F 1024 $rep/outs/possorted_bam.bam > $rep/outs/possorted_uniq_mapped_proper.bam
+
+echo "Counting number of genomic reads per droplet (potentially empty or with cell)"
+samtools view -@ $cpu $rep/outs/possorted_uniq_mapped_proper.bam | \
+   awk '{for (i=12; i<=NF; ++i) { if ($i ~ "^CB:Z") { print $i};} };' | \
+   sed -s "s/CB:Z://" | sort -S 20 | uniq -c | sed -s "s/  \+//" | \
+   awk '{ print $2 "\t" $1}' | sort -S 20 > $rep/outs/count_mapped_reads_per_barcode.tsv
+
+echo "Separating empty droplets from droplets with cell"
+# clean because we are going to append to this file
 rm -f $rep/outs/mapped_read_per_barcode.txt;
+
 # iterate over cellular barcodes
 for bc in `cat $rep/outs/filtered_peak_bc_matrix/barcodes.tsv`
 do 
-# extract them from BAM
-echo "echo $bc,\`samtools view -@ $cpu $rep/outs/possorted_uniq_mapped_proper.bam | grep CB:Z:${bc:0:16} | wc -l\` >> $rep/outs/mapped_read_per_barcode.txt"
+    echo "grep $bc $rep/outs/count_mapped_reads_per_barcode.tsv >> $rep/outs/mapped_read_per_barcode.txt"
 done | parallel -j $cpu
+
+
+echo "Done."
+
+exit 0
