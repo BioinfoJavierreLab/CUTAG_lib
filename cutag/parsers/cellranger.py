much faster normalization; fixed small bug in read matrix

cutag/parsers/cellranger.py
@@ -51,6 +51,22 @@ def get_total_mapped(rep):
     return elts
 
 
+def read_matrix(mpath, size):
+    fh = open(mpath)
+    count = 0
+    for line in fh:
+        if line.startswith('%') or line.startswith('#'):
+            count += len(line)
+            continue
+        break
+    fh.seek(count)
+    data, i, j = zip(*((int(v), int(i) - 1, int(j) - 1) 
+                            for i, j, v in (l.split()
+                                            for l in fh)))
+
+    return sparse.coo_matrix((data, (j, i)), shape=size).tocsc()
+
+
 def load_cellranger(directory, feature_type="peaks"):
     """
     :param directory: cellranger root output directory (containting the 'outs'
@@ -75,18 +91,8 @@ def load_cellranger(directory, feature_type="peaks"):
                open(os.path.join(directory, 'outs',
                                  ddir, 'barcodes.tsv'))]
 
-    values = np.zeros((len(columns), len(rows)))
-
-    fh = open(os.path.join(directory, 'outs',
-                           ddir, 'matrix.mtx'))
-    next(fh)
-    next(fh)
-    next(fh)
-    for line in fh:
-        a, b, v = line.split()
-        values[int(b) - 1, int(a) - 1] = int(v)
-
-    X = sparse.csr_matrix(values)
+    X = read_matrix(os.path.join(directory, 'outs', ddir, 'matrix.mtx'), 
+                    size=(len(columns), len(rows)))
 
     adata = ad.AnnData(X=X, obs=columns, var=rows, dtype=X.dtype)
 

scripts/normalize_sc-PRO.py
@@ -33,44 +33,36 @@ def merge_adjacent_bins(adata):
         preve = e
     list_of_lists.append(f"{prevc}:{prevb}-{e}")
     list_of_lists_idx.append(ns + [n + 1])
-
-    tmpX = adata.X.toarray()
-    tmp = np.vstack(tmpX[:,idx].sum(axis=1) for idx in list_of_lists_idx).T
     
-    bdata = ad.AnnData(X=tmp, obs=adata.obs)
+    adata.X = adata.X.tocsc()  # this will speed up column peeking
+    tmpX = adata.X.toarray()
+    tmp = np.vstack([tmpX[:,idx].sum(axis=1) for idx in list_of_lists_idx]).T
+    
+    bdata = ad.AnnData(X=tmp, obs=adata.obs, dtype=tmp.dtype)
     bdata.var_names = list_of_lists
 
     return bdata
 
 def normalize_TFIDF(adata, bgpath):
     # Get BG counts of genomic library
-    # Open the file and read in each line
-    bg_dict = {}
-    with open(bgpath), "r") as file:
-        for line in file:
-            # Split each line into two elements based on the whitespace separator
-            barcode, bg_count = line.split()
-            # Add the key-value pair to the dictionary
-            bg_dict[barcode] = int(bg_count)
-    
+    process_line = lambda barcode, bg_count: (barcode, int(bg_count)) 
+    bg_dict = dict(process_line(*l.split()) for l in open(bgpath, "r"))
     adata.obs["bg_counts"] = adata.obs.index.map(bg_dict)
 
-    # Filter bins in few cells
-    sc.pp.filter_genes(adata, min_cells=5)
-
     adata = merge_adjacent_bins(adata)
-
     # Normalize by TF-IDF
     N = len(adata.obs)
-    ni = adata.X.sum(0)
+    ni = adata.X.sum(axis=0)
     Cij = adata.X
-    Fj = adata.obs['bg_counts'][:,None]
+    Fj = adata.obs['bg_counts'].to_numpy()[:,None]
 
     adata.X = sparse.csr_matrix(
                     np.log(
                         (Cij / Fj) * (N / ni) * 10_000 + 1
                     )
-                ).tocoo()
+                ).tocsc()  # this will speed up most computation
+    # check!
+    print(adata.X.toarray().sum())
 
 
 def normalize_CLR(ad_adts):
@@ -89,32 +81,32 @@ def main():
 
     sample = opts.sample
     adt_by_bg = opts.adt_by_bg
+    outdir = opts.outdir
 
     cwd = "/home/xavi/Notebooks/Projects/scCUTandTAG/satija_processing/"
-    rwd = "/home/fransua/tmp/"
 
-    print(sample, "is running")
+    print(f"Processing {sample}")
 
-    results_dir = os.path.join(rwd, sample, sample + "_5000_tfidf_merged_bg")
-    os.system(f"mkdir -p {os.path.join(results_dir, 'outs', 'filtered_peak_bc_matrix')}")
-
-    ### load data
-
+    # Load data
     ### remove windows with less than five counts and then merge adjacent windows
-
     sample_dir = os.path.join(cwd, sample, sample + "_5000_notmerged")
 
+    print(f" - Loading Genomic library")
     adata = load_cellranger(sample_dir, feature_type="peaks")
 
+    # Filter bins in few cells
+    print(f" - Filter genomic features in few cells")
+    sc.pp.filter_genes(adata, min_cells=5)
+
     # Normalize by TF-IDF
+    print(f" - TF-IDF normalization on genomic library")
     normalize_TFIDF(adata, os.path.join(cwd, sample, "mapped_read_per_barcode.txt"))
 
     # Merge ADTS
+    print(f" - Loading ADTs")
     adts_sample = sample[:-3] + str(int(sample[-3:])+1)
     adts_dir = os.path.join(cwd, adts_sample)
-
     adts_file = glob(os.path.join(adts_dir, "*.tsv"))[0]
-
     # what is this?
     if sample.startswith("GSM"):
         ad_adts = sc.read(adts_file, delimiter=" ")
@@ -122,6 +114,7 @@ def main():
     else:
         ad_adts = sc.read(adts_file, delimiter="\t")
 
+    print(f" - Filter ADTs with few counts")
     sc.pp.filter_cells(ad_adts, min_counts=10)
 
     ad_adts.obs.index = [v.replace('.1', '-1') for v in ad_adts.obs.index]
@@ -130,26 +123,28 @@ def main():
     ad_adts.obs = pd.merge(ad_adts.obs, adata.obs, 
                            left_index=True, right_index=True)
 
+    print(f" - Normalize ADTs by CLR")
     normalize_CLR(ad_adts)
     
     # Normalize each cell ADT count by the genomic-library background
     if adt_by_bg:
-        ad_adts.X /= ad_adts.obs["bg_count"][:,None]
+        print(f" - Normalize ADTs by genomic background")
+        ad_adts.X /= ad_adts.obs["bg_counts"].to_numpy()[:,None]
 
     # Merge with MUON
-    adata.X = adata.X.tocsr()  # needed for the (useless) intersect
-
+    print(f" - Merge Genomic and ADT data into Muon object")
     mdata = mu.MuData({"histone": adata, "ADT": ad_adts})
 
     mdata.var_names_make_unique()
 
-    md_histones = mdata.mod["histone"]
-    md_membrane = mdata.mod["ADT"]
-
     mu.pp.intersect_obs(mdata)
     
-    
-    # TODO: save this
+    # save it
+    print(f" - Save Muon object")    
+    os.system(f"mkdir -p {outdir}")
+    mdata.write_h5mu(os.path.join(outdir, f"{sample}.h5ad"))
+
+    print(f"\nDone.")
 
 
 def get_options():
@@ -157,6 +152,8 @@ def get_options():
 
     parser.add_argument('-s', dest='sample', metavar='STR', required=True,
                         default=False, help='''Sample name''')
+    parser.add_argument('-o', dest='outdir', metavar='PATH', required=True,
+                        default=False, help='''Path to output folder.''')
     parser.add_argument('--bg', dest='adt_by_bg', action="store_true",
                         default=False, 
                         help='''Normalize ADTs by background from genomic-library''')